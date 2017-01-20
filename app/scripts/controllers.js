8ede5bd68a935da76dd46a4e5f2218c77c15bd7e
TommyKobryn <tom.kobryn@gmail.com>
2017-01-20 14:06:46 +0000

    Add files via upload

---
+++ app/scripts/controllers.js
@@ -0,0 +1,10 @@
+app.controller('commentController', function($scope) {
+  $scope.comments = [
+    {name:'Patricia' , title:'comment 1 Numbers or tingling in your feet Numbers or tingling in your feet Numbers or tingling in your feet'},
+    {name:'Jhonny' , title:'comment 2in your feet Numbers or tingling in your feet Numbers or tingling in your feet Numbers or tingling '},
+    {name:'Mark' , title:'comment 3 in your feet Numbers or tingling in your feet Numbers or tingling in your feet Numbers or tingling '},
+
+  ];
+
+
+});
