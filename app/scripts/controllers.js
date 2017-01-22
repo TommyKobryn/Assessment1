c9e10dd5f7f86a1dd88d7118c3299a28db4549f2
TommyKobryn <tom.kobryn@gmail.com>
2017-01-22 18:51:32 +0000

    Add files via upload

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -5,6 +5,20 @@ app.controller('commentController', function($scope) {
     {name:'Mark' , title:'comment 3 in your feet Numbers or tingling in your feet Numbers or tingling in your feet Numbers or tingling '},
 
   ];
+});
+
+app.controller('questionController', function($scope){
+
+$scope.questions = [
+  {name:'Eva' , title:'Will insuline make my patient gain weight?'},
+  {name:'Barbara' , title:'Q2 Will insuline make my patient gain weight?'},
+  {name:'Jenny' , title:'Q3 Will insuline make my patient gain weight?'},
+];
+});
 
 
+app.controller('ControllerJson', function($scope, $http) {
+  $http.get("http://www.pitbullsdogfood.com/montrail/db.json").then(function (response) {
+      $scope.data = response.data.records;
+  });
 });
